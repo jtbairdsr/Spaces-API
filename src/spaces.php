40506a83da705d0e725440f5522a9556807c5ba4
Jonathan Baird <jtbairdj@gmail.com>
2018-01-29 12:56:42 -0700

    remove unneeded verbose call to vendor/autoload.php

--- src/spaces.php
+++ src/spaces.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once(dirname(__FILE__) . "/../vendor/autoload.php"); // Autoload files using Composer autoload
-
 class SpacesConnect {
   /*
   An API wrapper for AWS, makes working with DigitalOcean's Spaces super easy.
